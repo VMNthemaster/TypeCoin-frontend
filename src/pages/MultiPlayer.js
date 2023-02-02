eab32170af34b775012745f2ee647557301c2208
VMNthemaster <varad.nadkarni09@gmail.com>
2023-02-02 23:42:39 +0530

    basic loading state ready and data available to all players in the room

--- src/pages/MultiPlayer.js
+++ src/pages/MultiPlayer.js
@@ -1,22 +1,34 @@
+import Loading from '../components/Loading'
 import React, { useEffect } from 'react'
 import useState from 'react-usestateref'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import { useStateContext } from '../context'
 import { checkEndingOfSentence, getRandomNumbers } from '../utils'
 import axios from 'axios'
+import { FaCarSide } from 'react-icons/fa'
 import io from 'socket.io-client'
 const socket = io.connect('http://localhost:5000')
 
 const MultiPlayer = () => {
   let { room } = useParams()
-  const {getSentences} = useStateContext()
+  const { getSentences } = useStateContext()
   const navigate = useNavigate()
   const { state } = useLocation()
-  // console.log(state)
   const [roomData, setRoomData] = useState(undefined)
   const [sentenceData, setSentenceData, getSentenceData] = useState({})
   const [loading, setLoading] = useState(true)
-
+  const [counter, setCounter] = useState(4)
+  const [currentPlayerNumber, setCurrentPlayerNumber] = useState('')
+  const [carMargin, setCarMargin] = useState({
+    player1: '0%',
+    player2: '0%',
+    player3: '0%',
+  })
+  const [wpm, setWpm] = useState({
+    player1: 0,
+    player2: 0,
+    player3: 0,
+  })
 
   const sendSentenceDataToBackend = async () => {
     const url = 'http://localhost:5000/getSentenceData'
@@ -26,7 +38,7 @@ const MultiPlayer = () => {
     })
 
     const data = await res.data
-    return data;
+    return data
   }
 
   const getSentenceDataFromBackend = async () => {
@@ -36,20 +48,17 @@ const MultiPlayer = () => {
     })
 
     const data = await res.data
-    return data;
+    return data
   }
 
-
   const getSentencesFromSmartContract = async () => {
-    const numArray = getRandomNumbers(1,state.roomData.numOfSentences, 1)
+    const numArray = getRandomNumbers(1, state.roomData.numOfSentences, 1)
 
-    const data = await getSentences(false, numArray)  // false since multiplayer
-    if(data.success){
+    const data = await getSentences(false, numArray) // false since multiplayer
+    if (data.success) {
       const parsedData = checkEndingOfSentence(data.data)
-      console.log(parsedData)
       setSentenceData(parsedData[0])
-    }
-    else{
+    } else {
       getSentencesFromSmartContract()
     }
 
@@ -58,25 +67,24 @@ const MultiPlayer = () => {
   }
 
   const joinRoom = () => {
-    socket.emit("join_room", {roomId: room})
+    socket.emit('join_room', { roomId: room })
   }
 
   useEffect(() => {
-    console.log("socket")
-
-    socket.on("set_loading_false", (data) => {
-      console.log(data)
-      setLoading(false)
+    socket.on('set_loading_false', (data) => {
+      if (data.roomId === room) {
+        // at this point everybody has all data
+        setLoading(false) // we can set it to false later as well
+      }
     })
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [socket])
-  
 
   useEffect(() => {
     if (state === null) {
       navigate('/username')
-      return;
+      return
     }
 
     setRoomData(state.roomData)
@@ -85,42 +93,67 @@ const MultiPlayer = () => {
 
     joinRoom()
 
-    if(state.roomData.currentRoomCount === 1){
+    if (state.roomData.currentRoomCount === 1) {
       getSentencesFromSmartContract()
-    }
-    else{
+    } else {
       const asyncGetSentenceDataFromBackend = async () => {
         const data = await getSentenceDataFromBackend()
-        console.log(data)
         setSentenceData(data)
+        setCurrentPlayerNumber(Object.keys(state.roomData.usernames)[state.roomData.currentRoomCount - 1])
       }
       asyncGetSentenceDataFromBackend()
     }
 
-    if(state.roomData.currentRoomCount === 3){
-      socket.emit("change_loading_state", {roomId: room}) 
+    if (state.roomData.currentRoomCount === 3) {
+      socket.emit('change_loading_state', { roomId: room })
     }
-
-
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
-    <div className={`h-[90vh]`}>
-      { roomData && (
-        <>
-          <div>{roomData.roomId}</div>
-          <div>{roomData.currentRoomCount}</div>
-          <div>{roomData.playState === false ? 'false' : 'true'}</div>
-          <div>
-            {roomData.usernames.player1}
-            {roomData.usernames.player2}
-            {roomData.usernames.player3}
+    <div
+      className={`h-[90vh] bg-gradient-to-b from-gray-100 to-gray-200 w-full flex justify-center items-center`}
+    >
+      {loading && <Loading />}
+
+      {!loading && (
+        <div className="card h-fit w-[40%] p-4 border-2 bg-white shadow-md rounded-md">
+          {/* heading */}
+          <div className="flex justify-between">
+            <h2 className="text-blue-500 text-lg font-semibold">
+              {counter > 0
+                ? 'The race is about to start...'
+                : 'The race has started!'}
+            </h2>
+            {counter >= 0 && (
+              <h2 className="text-lg font-semibold">: 0{counter}</h2>
+            )}
           </div>
-          {sentenceData && <div>{sentenceData.sentence}</div>}  
-          <div>{`${loading}` }</div>
-        </>
+
+          {/* car, there will be three cars */}
+          <div className="flex justify-between mt-2 items-center">
+            <div className={`w-[80%] h-fit`}>
+              <div style={{ marginLeft: carMargin }} className={`w-fit `}>
+                <FaCarSide className="inline" size={42} color="purple" />
+              </div>
+            </div>
+            <h2>{wpm.player1} wpm</h2>
+          </div>
+          <hr className="w-[80%] border-2 border-dashed border-red-400" />
+
+          <div
+            onClick={() => navigate('/')}
+            className="flex justify-start cursor-pointer mt-4"
+          >
+            <button className="bg-yellow-300 rounded-md hover:bg-yellow-400 text-blue-900 px-4 py-2">
+              Main Menu
+            </button>
+          </div>
+
+          {/* sentence and input */}
+          <div className="w-full p-4 mt-4 border-2 border-blue-300 bg-blue-100 flex flex-col gap-y-4 rounded-md"></div>
+        </div>
       )}
     </div>
   )
