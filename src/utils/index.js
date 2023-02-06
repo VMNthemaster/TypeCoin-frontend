af9c06670a6ba335b62c23b84d4927889c9bdf28
VMNthemaster <varad.nadkarni09@gmail.com>
2023-02-06 09:51:29 +0530

    landing page image fitting inside div

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,9 +2,8 @@ export const backgroundImageClasses = {
   backgroundImage:
     'url("https://cdn.pixabay.com/photo/2016/04/06/18/14/racing-1312447_960_720.png")',
   backgroundPosition: 'center',
-  backgroundSize: 'cover',
   backgroundRepeat: 'no-repeat',
-  // background: 'rgba(0, 0, 0, 0.5)',
+  backgroundSize: '100vw 90vh',
 }
 
 export const showAddress = (addr) => {
